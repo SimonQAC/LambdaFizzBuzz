3767644e203321bde5ea38b1ea2c69785314ad83
Simon Peppitt <simonworkpersonal@gmail.com>
2020-09-11 10:43:44 +0100

    initial commit

---
+++ src/com/qa/main/Main.java
@@ -0,0 +1,36 @@
+//thank you elizabeth for producing readable, understandable and replicatable code
+
+package com.qa.main;
+import java.util.ArrayList;
+
+public class Main {
+	
+    public interface FuncInterface {
+		Object operation(int a);
+    }
+    public static void operate(int a, FuncInterface funcObj) {
+        funcObj.operation(a);}
+   
+	
+	public static void main(String[] args) {
+	
+		FuncInterface fizzBuzz = (int x) -> {
+			String out = "";
+			if (x % 3 ==0) {out += "Fizz";}
+			if (x % 5 ==0) {out += "Buzz";}
+			if (out == "") {out = ""+ x;}
+			System.out.println(out);
+			return out;
+		};
+		
+		for (int i =1; i <= 100; i++) {
+			operate(i,fizzBuzz);
+		}
+
+	}
+	
+}
+
+
+
+
